719c78ab8f7fa4ee070c65c536a1a56eb9715918
JosuePercy <josuehuallullo12@gmail.com>
2025-04-19 11:47:32 -0500

    feat: hide WhatsApp number using dynamic API route to enhance privacy

---
+++ app/api/whatsapp-link/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+
+
+export async function GET(request: Request) {
+  const number = process.env.NEXT_PUBLIC_WSP_NUM || "944543773";
+  const baseNumber = number.startsWith("51") ? number : `51${number}`;
+    
+  const { searchParams } = new URL(request.url);
+  const product = searchParams.get("product") || "este producto";
+
+  const message = encodeURIComponent(
+    `🌹🌿☘ *¡Hola Valle del Mantaro!* Quiero información sobre ${product}`
+  );
+
+  const whatsappURL = `https://api.whatsapp.com/send?phone=${baseNumber}&text=${message}`;
+
+  return NextResponse.redirect(whatsappURL);
+}
